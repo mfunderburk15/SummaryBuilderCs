87f7e4f3eea832e9910af67ecbbc2bd794f3b941
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2021-11-06 20:07:33 -0500

    summary builder

---
+++ SummarizingTextCs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SummarizingTextCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "This is going to be a really really really really really long text.";
+            string summary = StringUtility.SummarizeText(sentence, 25);
+            Console.WriteLine(summary);
+        }
+
+        
+    }
+}
---
+++ SummarizingTextCs/StringUtility.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SummarizingTextCs
+{
+    public class StringUtility
+    {
+        public static string SummarizeText(string text, int maxLength = 20)
+        {
+            if (text.Length < maxLength)
+            {
+                return text;
+            }
+
+            string[] words = text.Split(' ');
+            int totalCharacters = 0;
+            List<string> summaryWords = new List<string>();
+
+            foreach (string word in words)
+            {
+                summaryWords.Add(word);
+
+                totalCharacters += word.Length + 1;
+                if (totalCharacters > maxLength)
+                {
+                    break;
+                }
+            }
+            string summary = String.Join(" ", summaryWords) + "...";
+            return summary;
+
+        }
+    }
+}
